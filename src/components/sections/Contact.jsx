5574b677dcaca1d92f78be28ab8594f9956acaed
Vikash-malviya <vikash.malviya2495@gmail.com>
2025-03-27 13:50:52 +0530

    Fixed dist directory issue

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -1,97 +1,77 @@
 import { useState } from "react";
 import { RevealOnScroll } from "../RevealOnScroll";
-import emailjs from "emailjs-com";
+import {AiFillGithub, AiFillLinkedin , AiFillInstagram} from 'react-icons/ai';
 
 export const Contact = () => {
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    message: "",
-  });
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    emailjs
-      .sendForm(
-        import.meta.env.VITE_SERVICE_ID,
-        import.meta.env.VITE_TEMPLATE_ID,
-        e.target,
-        import.meta.env.VITE_PUBLIC_KEY
-      )
-      .then((result) => {
-        alert("Message Sent!");
-        setFormData({ name: "", email: "", message: "" });
-      })
-      .catch(() => alert("Oops! Something went wrong. Please try again."));
-  };
-
+  
   return (
-    <section
-      id="contact"
-      className="min-h-screen flex items-center justify-center py-20"
-    >
-      <RevealOnScroll>
-        <div className="px-4 w-full min-w-[300px] md:w-[500px] sm:w-2/3 p-6">
-          <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
-            {" "}
-            Get In Touch
-          </h2>
-          <form className="space-y-6" onSubmit={handleSubmit}>
-            <div className="relative">
-              <input
-                type="text"
-                id="name"
-                name="name"
-                required
-                value={formData.name}
-                className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
-                placeholder="Name..."
-                onChange={(e) =>
-                  setFormData({ ...formData, name: e.target.value })
-                }
-              />
-            </div>
-
-            <div className="relative">
-              <input
-                type="email"
-                id="email"
-                name="email"
-                required
-                value={formData.email}
-                className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
-                placeholder="example@gmail.com"
-                onChange={(e) =>
-                  setFormData({ ...formData, email: e.target.value })
-                }
-              />
-            </div>
-
-            <div className="relative">
-              <textarea
-                id="message"
-                name="message"
-                required
-                rows={5}
-                value={formData.message}
-                className="w-full bg-white/5 border border-white/10 rounded px-4 py-3 text-white transition focus:outline-none focus:border-blue-500 focus:bg-blue-500/5"
-                placeholder="Your Message..."
-                onChange={(e) =>
-                  setFormData({ ...formData, message: e.target.value })
-                }
-              />
-            </div>
-
-            <button
-              type="submit"
-              className="w-full bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]"
-            >
-              Send Message
-            </button>
-          </form>
+   <section className="py-8" id="contact">
+    <div className="max-w-[1200px] mx-auto px-4">
+      <div className="flex items-center justify-between flex-col md:flex-row ">
+        <div>
+        <h2 className="md:text-6xl text-3xl font-bold mb-10 text-white/50">
+        Get in <span className="text-gray-400">Touch</span>
+        </h2>
+        <a 
+        className="md:text-5xl text-3xl font-semibold text-white underline decoration-gray-400 decoration-2 
+        underline-offset-4 hover:decoration-gray-200 transition duration-300"
+         href="mailto:vikash.malviya@2495gmail.com" >
+          vikash.malviya@gmail.com
+        </a>
         </div>
-      </RevealOnScroll>
-    </section>
+
+        <div className="text-white/50 mt-12">
+        <div className="mb-8">
+          <p className="text-lg mb-1 ">Phone</p>
+          <a href="+919929691872" 
+          className="text-2xl font-semibold underline decoration-gray-400 decoration-2 underline-offset-4 hover:decoration-gray-400 transition duration-300"
+          >
+            +91 9929691872
+          </a>
+        </div>
+        <div className="text-lg mb-8">
+          <p className="font-bold">Home</p>
+          <p>Hadiya Chowk</p>
+          <p>Sojat City (306104)</p>
+          <p>Rajasthan</p>
+        </div>
+
+        </div>
+
+      </div>
+      
+
+      <div className="relative mt-12 container flex sm:justify-between justify-center items-center gap-10 max-sm:flex-col">
+        <p className="text-gray-200 caption text-n-4 lg:block">
+         © 2025. All right reserved.
+        </p>
+
+        <ul className="flex gap-5 flex-wrap">
+          <a href="https://github.com/"
+          aria-label="GitHub"
+          className="text-gray-200 flex items-center justify-center w-10 h-10 rounded-full" 
+          >
+            <AiFillGithub size={30}/>
+          </a>
+
+          <a href="https://www.linkedin.com/in/vikash-030337307/"
+          aria-label="Linkedin"
+          className="text-gray-200 flex items-center justify-center w-10 h-10 rounded-full" 
+          >
+            <AiFillLinkedin size={30}/>
+          </a>
+
+          <a href="https://www.instagram.com/_vikash__malviya___/?hl=en"
+          aria-label="Instagram"
+          className="text-gray-200 flex items-center justify-center w-10 h-10 rounded-full" 
+          >
+            <AiFillInstagram size={30}/>
+          </a>
+        </ul>
+      </div>
+
+    </div>
+
+   </section>
   );
 };
